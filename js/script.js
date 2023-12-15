801885144136b1032cfd2de954a71db9b95d8fcb
gopinathreddymygapula <153773078+gopinathreddymygapula@users.noreply.github.com>
2023-12-15 07:16:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-j
+document.addEventListener("DOMContentLoaded", function () {
+    // Highlight the active navigation link based on the current URL
+    highlightActiveNavLink();
+
+    function highlightActiveNavLink() {
+        var currentPage = window.location.pathname.split("/").pop();
+        var navLinks = document.querySelectorAll("nav a");
+
+        navLinks.forEach(function (link) {
+            var linkHref = link.getAttribute("href").split("/").pop();
+
+            if (linkHref === currentPage) {
+                link.classList.add("active");
+            }
+        });
+    }
+});
+
